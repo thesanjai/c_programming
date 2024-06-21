348586f23464d2bc13348529398c3817924713db
SANJAI-909 <sanjaisk83@gmail.com>
2024-06-21 14:59:47 +0000

    10 programs completd

---
+++ 1_sem_c_practise/SIMPLE PROGRAMS/10.c
@@ -0,0 +1,30 @@
+//Quadratic equation 
+#include<stdio.h> 
+#include<math.h> 
+
+int main(){
+    float a,b,c;
+    printf("Enter the (a,b,c): ");
+    scanf("%f%f%f",&a,&b,&c);
+    /*
+        x1,x2= -b (+-)sqrt(b^2 -4ac)
+                ____________________
+                        2a
+    */
+    float det=((b*b)- (4*a*c));
+
+    if(det < 0){
+        printf("Imaginary roots\n");
+        printf("cannot able to find the root\n");
+        return 0;
+    }else if(det >0){
+        printf("REAL roots\n");
+    }else{
+        printf("real & equal roots\n");
+    }
+
+    float x1=( (-b)+sqrt(det) )/(2*a);
+    float x2=( (-b)-sqrt(det) )/(2*a);
+    printf("The roots are %f and %f \n",x1,x2);
+    return 0;
+}
