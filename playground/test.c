4ea4cb6415d8a0dcb4c71cbb2b549b1dfdf3cfe6
SANJAI-909 <sanjaisk83@gmail.com>
2024-06-24 17:43:22 +0000

    memory hook learned

--- playground/test.c
+++ playground/test.c
@@ -1,15 +1,5 @@
 #include<stdio.h>
-#include<unistd.h>
-#include<time.h>
-#include<math.h>
-
-
-
 int main(){
-	int a,b;
-
-	printf("Enter a and b : ");
-	scanf("%d%d",&a,&b);
-
-	printf("%f",pow(a,b));
+	printf("Hello world\n");
+	printf("LAHTP");
 }
