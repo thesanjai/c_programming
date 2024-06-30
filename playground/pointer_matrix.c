b6836585ba7aeed1b4a839bb0bd268a4e8b1fd8f
SANJAI-909 <sanjaisk83@gmail.com>
2024-06-30 04:59:03 +0000

    pointer matrix notes added

--- playground/pointer_matrix.c
+++ playground/pointer_matrix.c
@@ -14,7 +14,7 @@ int* display(int* a,int row,int col ){
 
 int main(){
     
-    int a[3][4]={
+    int a[][4]={   //we must give col size 
                 {1,2,3,4},
                 {5,6,7,8}, 
                 {9,10,11,12}
