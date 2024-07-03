4b5eaf00faefa8ed93c93d49a24632195f593835
SANJAI-909 <sanjaisk83@gmail.com>
2024-07-03 08:44:31 +0000

    testing code in quickref.me

--- playground/test.c
+++ playground/test.c
@@ -1,22 +1,21 @@
 #include<stdio.h>
+
+typedef struct rectangle{
+	int length,breadth;
+}rectangle;
+
+void create(rectangle *r);
+
 int main(){
 
-	char name1[7],name2[100]="Hello world  i am not good girl \n";
+	rectangle r;
+	create(&r);
 
-	printf("[before 1st scanf()] name2 = %s\n",name2);
-	printf("enter the name 1 :");
-	scanf("%s",name1);
-	printf("[after 1st scanf()] name2 = %s\n",name2);
+	printf("area of rectangle: %d\n",r.length*r.breadth);
+	return 0;
+}
 
-	printf("enter the name 2 :");
-	scanf("%s",name2);
-
-	printf("name\b\b 1 : %s\n",name1);
-	printf("name 2 : %s\n",name2);
-
-
-	for(int i=0;i<50;i++){
-		printf("%c %d\n",name1[i],name1[i]);
-	}
-
+void create(rectangle *r){
+	(*r).length=10;
+	r->breadth=20;
 }
