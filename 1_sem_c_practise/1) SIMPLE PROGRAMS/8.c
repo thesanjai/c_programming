49357e202ac1fb0e5d939a1cf8162116919e371f
SANJAI-909 <sanjaisk83@gmail.com>
2024-07-06 09:20:17 +0530

    removed

--- 1_sem_c_practise/1) SIMPLE PROGRAMS/8.c
+++
@@ -1,9 +0,0 @@
-//Leap year checking
-#include <stdio.h>
-int main(){
-    int n;
-    printf("Enter the year: ");
-    scanf("%d",&n);
-    printf("%d is %s",n,n%4==0?(n%100==0?(n%400==0?"leap year":"not a leap year"):"leap year"):"not a leap year");
-    return 0;
-}
