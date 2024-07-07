997562b4e028cf6572fa3c546e4c1cf32c4dcefa
SANJAI-909 <sanjaisk83@gmail.com>
2024-07-07 07:33:22 +0000

    mat add sub mul trans tested

---
+++ 1_sem_c_practise/3) PROGRAMMS USING ARRAYS/MATRIX/46.c
@@ -0,0 +1,55 @@
+#include "matrix_h.h"
+
+
+int main(){
+
+    matrix a,b;
+
+    create_matrix(&a);
+    create_matrix(&b);
+
+    matrix *result = add_matrix(a,b);
+
+    matrix *result1 = sub_matrix(a,b);
+
+    matrix *result2 = mul_matrix(a,b);
+
+    matrix *result3 = trans_matrix(a);
+
+    print_matrix(a);
+    print_matrix(b);
+
+    if(result == NULL){
+        printf("Error ....ADDITION OPEARTION NOT PERFORMED\n");
+    }else{
+        print_matrix(*result);
+    }
+
+    if(result1 == NULL){
+        printf("Error ....SUBTRACTION OPEARTION NOT PERFORMED\n");
+    }else{
+        print_matrix(*result1);
+    }
+
+    if(result2 == NULL){
+        printf("Error ....MULTIPLICATION OPERATION NOT PERFORMED\n");
+    }else{
+        print_matrix(*result2);
+    }
+
+     if(result3 == NULL){
+        printf("Error ....TRANSPOSE OPERATION NOT PERFORMED\n");
+    }else{
+        print_matrix(*result3);
+    }
+
+
+
+   /*free_matrix_memory(&a);
+    free_matrix_memory(&b);
+    free_matrix_memory(result);
+    free_matrix_memory(result1);
+    free_matrix_memory(result1);
+    free_matrix_memory(result3);*/
+    return 0;
+}
