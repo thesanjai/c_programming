776c0b02b690ba9208f9cb51db1f1566c4064c18
SANJAI-909 <sanjaisk83@gmail.com>
2024-07-14 04:40:12 +0000

    test Bin to oct; oct to bin

--- 1_sem_c_practise/4) PROGRAMMS USING FUNCTIONS/test.c
+++ 1_sem_c_practise/4) PROGRAMMS USING FUNCTIONS/test.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    printf("%10c elloword",'\0');
+    int n;
+    printf("Enter the n: ");
+    scanf("%d",&n);
+
+    int ans=0;
+    while(n != 0){
+        ans = ans *10 +n%10;
+        n = n/10;
+    }
+
+    printf("Reversed number: %d\n",ans);
 }
