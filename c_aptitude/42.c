b9b166319ce07e412bebe52a89c2484d301cce50
SANJAI-909 <sanjaisk83@gmail.com>
2024-07-15 12:14:51 +0000

    comma operator precedence

---
+++ c_aptitude/42.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int a=1,b=2,c=3;
+    char d=0;
+
+    if(a,b,c,d){
+        printf("sanjai\n");
+    }else{
+        printf("sivakumar\n");
+    }
+    return 0;
+}
+/* comma operators evaluates all from left to right and return the right most value
+*/
