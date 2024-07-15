0a988b49cf81d9a3c592ca641e31a57b96db5773
SANJAI-909 <sanjaisk83@gmail.com>
2024-07-15 12:33:49 +0000

    printf() function

---
+++ c_aptitude/44.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    if(printf("coding ")){
+        switch(printf("is ")){
+            while(printf("fun\n")){
+
+            }
+        }
+    }
+    return 0;
+}
+/*NOTES
+    printf function returns the length of the string which prints
+    in this case, it returns the 7 in if statement
+    then 3 is passed to switch, then compiler searches for case which is matches with 3, here there
+    is no such case is given, so it terminates in the switch case itself, so while loop will not execute
+*/
