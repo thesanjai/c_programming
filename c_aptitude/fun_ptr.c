328f10a3e41884bb136c486853e7d44bec4b87fe
SANJAI-909 <sanjaisk83@gmail.com>
2024-07-29 14:17:28 +0000

    another example of function pointer

--- c_aptitude/fun_ptr.c
+++ c_aptitude/fun_ptr.c
@@ -16,3 +16,29 @@ int main(){
     printf("Multiplication: %d\n",fun(5,3));
     return 0;
 }
+/*#include <stdio.h>
+
+// Function pointer type
+typedef void (*func_ptr)(int);
+
+// Functions to point to
+void foo(int x) { printf("foo: %d\n", x); }
+void bar(int x) { printf("bar: %d\n", x); }
+
+int main() {
+    // Create a function pointer
+    func_ptr ptr = foo;
+
+    // Call the pointed-to function
+    ptr(10); // Output: foo: 10
+
+    // Change the pointer to point to another function
+    ptr = bar;
+
+    // Call the new pointed-to function
+    ptr(20); // Output: bar: 20
+
+    return 0;
+}
+
+*/
